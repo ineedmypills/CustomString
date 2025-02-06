92974be62402291021eb4a4afcffd840a47ef5ea
ElNefro <Yanik0206@gmail.com>
2025-02-06 09:58:02 +0500

    Добавьте файлы проекта.

---
+++ MyString.hpp
@@ -0,0 +1,14 @@
+#ifndef _MY_STRING_HPP_
+#define _MY_STRING_HPP_
+#include <iostream>
+#include <string.h>
+
+class MyString
+{
+public:
+private:
+	char* str = nullptr;
+	unsigned int size{ 0 };
+};
+
+#endif	//!_MY_STRING_HPP_
